c5e41c502fc6ca18e509778f9fb0019a7dcc7c53
rfillon <107402916+RomainFillon@users.noreply.github.com>
2023-07-31 13:19:31 +0200

    Update f_model_tp.py

--- model/f_model_tp.py
+++ model/f_model_tp.py
@@ -62,45 +62,25 @@ def emissions(ab_level,production,carbon_intensity):#emissions per period
     e=carbon_intensity*(1-ab)*production
     return e;
 
-def partialy_emissions(ab_level,production,carbon_intensity):#emissions per period, partially derived with respect to y
-    ab=maximum(ab_level,0)
-    ab=minimum(ab,1)
-    e=carbon_intensity*(1-ab)
-    return e;
-
 def temperature(cum_e):
     v=beta_temp*cum_e
     return v;
-
-def partials_temperature(cum_e):#temperature, partially derived with respect to cumulative emissions
-    v=beta_temp
-    return v;
-
+    
 #state of the environment as a function of temperature
+#function to be used if the instantaneous utility function is as in Sterner & Persson (2008)
 def environment_from_temperature(temp):
     E=E0/(1+ae*temp**2)
     return E;
 
 def damage_factor_pretipping(temp,eps):#damages factor, correcting TFP, from temperature and shock (caution: positive shock=more productivity=less damages)
-    #omega=maximum(10**(-8), 1 /(1+pi1*temp+pi2*temp**pi3)-eps)
-    # nouvelle forme (Nordhaus, 2016) 
     omega=maximum(10**(-8), 1-(pi1*temp+pi2*temp**pi3)-eps)
     return omega;
 
 def damage_factor_posttipping(temp,eps):#damages factor, correcting TFP, from temperature and shock (caution: positive shock=more productivity=less damages)
-    #omega=maximum(10**(-8), (1-J) /(1+pi1*temp+pi2_tp*temp**pi3)-eps)
-    #omega=maximum(10**(-8), (1) /(1+pi1*temp+J*(temp-threshold_min)**pi3+pi2*threshold_min**2)-eps)        
-    # nouvelle forme (Nordhaus 2016)
     omega=maximum(10**(-8), (1-J)*(1-(pi1*temp+pi2*temp**pi3))-eps)
     return omega;
 
-
-def partialt_damage_factor(temp,eps):#damages factor, partially derived with respect to temperature
-    #omega=-(pi1+pi2*pi3*temp**(pi3-1)) /((1+pi1*temp+pi2*temp**pi3)**2)
-    # nouvelle forme (Nordhaus 2016)
-    omega=-(pi1+pi2*pi3*temp**(pi3-1))
-    return omega;
-
+#proba tipping
 def h(S1,S2,Smin,Smax):
     zeromat=zeros(shape(S1))
     onemat=ones(shape(S1))
